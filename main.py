7ac11b382051d5b9a0ebb6147a7ec0e0129f4114
amnad07071983 <amnad.tho@jppartner2018.com>
2026-04-13 23:25:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def create_pdf(df, sheet_name):
     def add_watermark(c: canvas.Canvas, doc):
         try:
             c.saveState()
-            c.setFillAlpha(0.05)
+            c.setFillAlpha(0.2)
 
             width, height = A4
             img_width = 140 * mm
